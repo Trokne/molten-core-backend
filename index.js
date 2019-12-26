f1e56a1ed93dbf3ff6f09f3c989f4d36b99e33f7
Mikhail Vshivkov <m.vshivkov@xsolla.com>
2019-12-26 11:28:09 +0500

    Fix URL

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-  });
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
