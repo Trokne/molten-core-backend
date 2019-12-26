15fc80ec684f13123c117db8b090c197d2451044
Mikhail Vshivkov <m.vshivkov@xsolla.com>
2019-12-26 11:50:53 +0500

    Xsolla test webhooks

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get("/items", (req, res, next) => {
-    res.status(204);
-    res.json('');
+  // .get("/items", (req, res, next) => {
+  //   res.status(204);
+  //   res.json('');
+  // })
+  .post("/items", (req, res, next) => {
+    if (req.notification_type == "user_validation") {
+        if (req.user.id === 1 || req.user.id === 2 || req.user.id === 12) {
+          res.status(200);
+          return;
+        }
+        else { 
+          res.status(404);
+          res.json('INVALID USER');
+          return;
+      }
+    }
+
+    if (req.notification_type == "payment") {
+      if (req.user.id === 1 || req.user.id === 2 || req.user.id === 12) {
+        res.status(200);
+      }
+      else { 
+        res.status(404);
+        res.json('INVALID USER');
+      }
+
+    
+  }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
