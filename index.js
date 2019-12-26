00b3f54f817667360298f9690aef6f4a43043aa4
Mikhail Vshivkov <m.vshivkov@xsolla.com>
2019-12-26 11:25:39 +0500

    Test url

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+  });
